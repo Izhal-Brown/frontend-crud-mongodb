d295b0d75aca0427e63f459d5ccc227e8f6e1772
Izhal_Brown <30791244+Izhal-Brown@users.noreply.github.com>
2022-10-26 13:41:41 +0800

    Update index.jsx

--- src/pages/Detail/index.jsx
+++ src/pages/Detail/index.jsx
@@ -14,7 +14,7 @@ const Detail = () => {
   },[])
 
   const getProductById = async (e) => {
-    const response = await axios.get(`https://backend-crud-m.herokuapp.com/api/v1/product//${id}`);
+    const response = await axios.get(`https://backend-crud-m.herokuapp.com/api/v1/product/${id}`);
     setProduct(response.data)
   }
 
